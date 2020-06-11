d14574b07958502bcc88292fb4b5406fb0f5ca6d
Luke Mainwaring <lukemainwaring@Lukes-MacBook-Pro-3.local>
2020-06-10 23:08:53 -0400

    basic styling and functionality for create game button

--- src/components/Lobby.js
+++ src/components/Lobby.js
@@ -1,21 +1,37 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
+import { Button, Grid, Typography, withStyles } from '@material-ui/core';
 
 import { createGame } from '../actions/games';
 
-const Lobby = ({ createGame }) => {
+const Lobby = ({ classes, createGame, words }) => {
   return (
     <div>
-      <button className='ui button primary' onClick={() => createGame()}>
-        Create New Game
-      </button>
+      <Typography variant='h1' component='h2' align='center'>
+        Theme Song
+      </Typography>
+      <Grid alignItems='center' container justify='center' spacing={2}>
+        <Grid item>
+          <Button
+            className={classes.createGameButton}
+            color='primary'
+            variant='contained'
+            onClick={() => createGame()}
+            size='large'
+          >
+            Create New Game
+          </Button>
+        </Grid>
+      </Grid>
+
+      <span>{words}</span>
     </div>
   );
 };
 
-const mapStateToProps = ({}) => {
-  return {};
+const mapStateToProps = ({ games }) => {
+  return { words: games.words };
 };
 
 const mapDispatchToProps = (dispatch) => {
@@ -27,4 +43,14 @@ const mapDispatchToProps = (dispatch) => {
   );
 };
 
-export default connect(mapStateToProps, mapDispatchToProps)(Lobby);
+const styles = {
+  createGameButton: {
+    justifyContent: 'right',
+    // paddingLeft: 40,
+  },
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(withStyles(styles)(Lobby));
--- src/reducers/gameReducer.js
+++ src/reducers/gameReducer.js
@@ -1,14 +1,13 @@
 import { CREATE_GAME } from '../actions/games';
 
 const INITIAL_STATE = {
-  words: [],
+  words: '',
 };
 
 export default (state = INITIAL_STATE, action) => {
   switch (action.type) {
     case CREATE_GAME:
-      console.log(action.payload);
-      return { ...state, words: action.payload };
+      return { ...state, words: action.payload.words };
     default:
       return state;
   }
